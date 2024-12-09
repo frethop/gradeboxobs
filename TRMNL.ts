2ca55e15be350a79f294191da17e882ba2e3cb8f
frethop <jipping@hope.edu>
2024-12-09 20:35:48 +0000

    12-9-9

--- TRMNL.ts
+++ TRMNL.ts
@@ -15,6 +15,24 @@ export class TRMNL {
     }
 
     publish(extrastuff: string): boolean {
+        let url = "https://usetrmnl.com/api/custom_plugins/"+this.pluginID;
+        let variables = {"merge_variables": {"clsss": extrastuff, "html": this.html }}
+        fetch(url, {
+            method: "POST", 
+            body: JSON.stringify(variables),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+            .then(response => response.text())
+            .then(html => {
+                // Do something with the HTML content
+                console.log(html); 
+            })
+        .catch(error => {
+            console.error('Error fetching HTML:', error);
+        });
+
         return true;
     }
 }
